e85625b70569cfdce18e72dce5364ef10c767287
Ushant Singh <ushantsingh4@gmail.com>
2025-12-24 21:28:20 +0530

    2nd Project

---
+++ script.js
@@ -0,0 +1,27 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const results = document.querySelector('#results');
+
+    if (height === '' || height < 0 || isNaN(height)) {
+        results.innerHTML = `Please enter a Valid height ${height}`;
+    } else if (weight === '' || weight < 0 || isNaN(weight)) {
+        results.innerHTML = `Please enter a Valid Weight ${weight}`;
+    } else {
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+        // show the result 
+        results.innerHTML = `<span>${bmi}</span>`;
+
+        if (bmi < 18.6) {
+            results.innerHTML = `Under Weight = ${bmi}`;
+        } else if (bmi == 18.6 || bmi <= 24.9) {
+            results.innerHTML = `Normal Weight = ${bmi}`;
+        } else if(bmi > 24.9){
+            results.innerHTML = `Overweight = ${bmi}`;
+        }
+    }
+});
