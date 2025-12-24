2219d424b7812711bd8c787d8c1827913a335e08
Ushant Singh <ushantsingh4@gmail.com>
2025-12-24 21:29:39 +0530

    t

--- script.js
+++ script.js
@@ -20,7 +20,6 @@ form.addEventListener('submit', function (e) {
             results.innerHTML = `Under Weight = ${bmi}`;
         } else if (bmi == 18.6 || bmi <= 24.9) {
             results.innerHTML = `Normal Weight = ${bmi}`;
-            
         } else if(bmi > 24.9){
             results.innerHTML = `Overweight = ${bmi}`;
         }
